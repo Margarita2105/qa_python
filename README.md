01d02a86f75f1f6f2f6217e72a86a1327ec760d9
Margarita Khokhlova <khokhlova2105@yandex.ru>
2024-12-15 21:00:52 +0300

    first commit sprint 4

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # qa_python
+test_add_new_book_add_two_books - Проверяет добавление двух книг в словарь books_rating. Используемые методы - add_new_book и get_books_rating
+
+test_add_new_book_book_has_no_genre - Проверяет, что у добавленных книг нет жанра. Используемые методы - add_new_book и get_book_genre
+
+test_set_book_genre_name_corresponds_genre - Проверяет, что книге добавлен жанр и он соответствует добавленному жанру. Используемые методы - add_new_book, set_book_genre и get_book_genre
+
+test_get_books_with_specific_genre_get_the_fiction_genre - Проверяет, что отбираются книги с жанром Фантастика. Используемые методы - add_new_book, set_book_genre и get_books_with_specific_genre
+
+
+test_get_books_with_specific_genre_name_corresponds_genre - Проверяет, что выводится название книги с заданным жанром. Используемые методы - add_new_book, set_book_genre и get_books_with_specific_genre
+
+test_get_books_for_children_only_children_genre - Проверяет, что возвращаются книги с разрешенным жанром для детей. Используемые методы - add_new_book, set_book_genre и get_books_for_children
+
+test_get_books_for_children_not_children_genre - Проверяет, что не возвращаются книги с запрещенным жанром для детей. Используемые методы - add_new_book, set_book_genre и get_books_for_children
+
+test_add_book_in_favorites_favorites_name_in_list - Проверяет, что возвращается название книги, добавленное в избранное. Используемые методы - add_new_book, add_book_in_favorites и get_list_of_favorites_books
+
+test_add_book_in_favorites_only_favorites_book_in_list - Проверяет, что в избранном только книги, добавленные в избранное. Используемые методы - add_new_book, add_book_in_favorites и get_list_of_favorites_books
+
+
+test_delete_book_from_favorites_delete_one_book - Проверяет, что при удалении книги из избранного, книга из избранного удаляется. Используемые методы - add_new_book, add_book_in_favorites, delete_book_from_favorites и get_list_of_favorites_books
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from main import BooksCollector
+# noinspection PyUnresolvedReferences
+import pytest
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -20,5 +22,81 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
+    def test_add_new_book_book_has_no_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость')
+
+        assert collector.get_book_genre('Гордость') == ''
+
+    @pytest.mark.parametrize('name, genre',
+                             [['Черновик', 'Фантастика'], ['Гордость', 'Детективы'], ['Недотепа', 'Мультфильмы'],
+                              ['Кошмар', 'Ужасы'], ['Смешно', 'Комедии']])
+    def test_set_book_genre_name_corresponds_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.get_book_genre(name) == genre
+
+    def test_get_books_with_specific_genre_get_the_fiction_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Черновик')
+        collector.add_new_book('Гордость')
+        collector.add_new_book('Недотепа')
+        collector.set_book_genre('Черновик', 'Фантастика')
+        collector.set_book_genre('Гордость', 'Детективы')
+        collector.set_book_genre('Недотепа', 'Фантастика')
+
+        assert len(collector.get_books_with_specific_genre('Фантастика')) == 2
+
+    @pytest.mark.parametrize('name, genre', [['Черновик', 'Фантастика'], ['Гордость', 'Детективы'], ['Недотепа', 'Мультфильмы'], ['Кошмар', 'Ужасы'], ['Смешно', 'Комедии']])
+    def test_get_books_with_specific_genre_name_corresponds_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert collector.get_books_with_specific_genre(genre)[0] == name
+
+    @pytest.mark.parametrize('name, genre', [['Черновик', 'Фантастика'], ['Недотепа', 'Мультфильмы'], ['Смешно', 'Комедии']])
+    def test_get_books_for_children_only_children_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert len(collector.get_books_for_children()) > 0
+
+    @pytest.mark.parametrize('name, genre', [['Гордость', 'Детективы'], ['Кошмар', 'Ужасы']])
+    def test_get_books_for_children_not_children_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+
+        assert len(collector.get_books_for_children()) == 0
+
+    def test_add_book_in_favorites_favorites_name_in_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость')
+        collector.add_book_in_favorites('Гордость')
+
+        assert collector.get_list_of_favorites_books()[0] == 'Гордость'
+
+    def test_add_book_in_favorites_only_favorites_book_in_list(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость')
+        collector.add_new_book('Кошмар')
+        collector.add_book_in_favorites('Гордость')
+
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites_delete_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость')
+        collector.add_new_book('Кошмар')
+        collector.add_book_in_favorites('Гордость')
+        collector.add_book_in_favorites('Кошмар')
+        collector.delete_book_from_favorites('Гордость')
+
+        assert len(collector.get_list_of_favorites_books()) == 1
+
     # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector
